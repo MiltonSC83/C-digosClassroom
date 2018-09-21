0208098f1fb626f42fbdaf02ecf57b3c1c085bf2
MiltonSC83 <42243730+MiltonSC83@users.noreply.github.com>
2018-09-20 20:32:44 -0500

    boton

---
+++ Interfaz/Interfaz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transacciones t1 = new Transacciones("0001", "20/09/2018", 500000.00);
+            Transacciones t2 = new Transacciones("0002", "21/09/2018", 250000.00);
+
+            t1.muestraTransacciones();
+            t2.muestraTransacciones();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Interfaz/Interfaz/Transacciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz
+{
+    class Transacciones: ITransacciones
+    {
+        private string codigoT;
+        private string fecha;
+        private double monto;
+
+        public Transacciones()
+        {
+            codigoT = "";
+            fecha = "";
+            monto = 0.0;
+        }
+
+        public Transacciones(string c, string f, double m)
+        {
+            codigoT = c;
+            fecha = f;
+            monto = m;
+        }
+
+        public void muestraTransacciones()
+        {
+            Console.WriteLine("Transacción: {0}", codigoT);
+            Console.WriteLine("Data: {0}", fecha);
+            Console.WriteLine("Monto: {0}", getmonto());
+        }
+        public double getmonto()
+        {
+            return monto;
+        }
+    }
+}
